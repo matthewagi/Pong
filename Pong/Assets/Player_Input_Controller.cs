d351405e96b1302a63d97ab6378a45a27de98167
matthewagi <matthewagi@gmail.com>
2018-04-05 09:26:43 +0200

    Player Controller
    Setting up a player controller

---
+++ Pong/Assets/Player_Input_Controller.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Input_Controller : MonoBehaviour {
+    public GameObject Left_Bat;
+    public GameObject Right_Bat;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Bat goes up
+if(Input.GetKey (KeyCode.W))
+        {
+            Debug.Log("Pressing W");
+        }
+
+//Bat Goes down
+else if (Input.GetKey(KeyCode.S))
+        {
+            Debug.Log("Pressing S");
+        }
+
+    }
+}
