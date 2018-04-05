105b38e8c602b7d71c0665ac73d3f79d4f267998
matthewagi <matthewagi@gmail.com>
2018-04-05 09:46:44 +0200

    Basic Controls

--- Pong/Assets/Player_Input_Controller.cs
+++ Pong/Assets/Player_Input_Controller.cs
@@ -12,17 +12,48 @@ public class Player_Input_Controller : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+
+        //Left Bat
+
+
+        // keeps from having other movements at the same time
+        Left_Bat.GetComponent<Rigidbody>().velocity = new Vector3(0f, 0f, 0f);
         //Bat goes up
-if(Input.GetKey (KeyCode.W))
+        if (Input.GetKey (KeyCode.W))
         {
             Debug.Log("Pressing W");
+            Left_Bat.GetComponent<Rigidbody>().velocity = new Vector3(0f, 4f, 0f);
         }
 
 //Bat Goes down
 else if (Input.GetKey(KeyCode.S))
         {
             Debug.Log("Pressing S");
+            Left_Bat.GetComponent<Rigidbody>().velocity = new Vector3(0f, -4f, 0f);
         }
+
+
+
+        //Right Bat
+
+
+        Right_Bat.GetComponent<Rigidbody>().velocity = new Vector3(0f, 0f, 0f);
+        //Bat goes up
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+         
+            Right_Bat.GetComponent<Rigidbody>().velocity = new Vector3(0f, 4f, 0f);
+        }
+
+        //Bat Goes down
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            
+            Right_Bat.GetComponent<Rigidbody>().velocity = new Vector3(0f, -4f, 0f);
+        }
+
+
 
     }
 }
